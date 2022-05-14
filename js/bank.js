39cb72499270e338c7cdb49d8af01136d17a9b25
Abdus Sobur Shihab <abdussoburshihab56@gmail.com>
2022-05-14 13:10:34 +0600

    withdraw

--- js/bank.js
+++ js/bank.js
@@ -1,17 +1,35 @@
 /// handle deposit btn event
 document.getElementById('depo-btn').addEventListener('click',function(){
     const depoinput=document.getElementById('deposit');
-    const newdepoAmount = depoinput.value;
-    console.log(newdepoAmount);
+    const newdepoAmountText = depoinput.value;
+    const newdepoAmount=parseFloat(newdepoAmountText);
+    // console.log(newdepoAmount);
     const deposittotal=document.getElementById('depo-total');
-    console.log(deposittotal.innerText);
-   const previousdepoAmount=deposittotal.innerText;
-   const newdepototal=previousdepoAmount +newdepoAmount;
+    // console.log(deposittotal.innerText);
+    const previousdepoText=deposittotal.innerText;
+   const previousdepoAmount=parseFloat(previousdepoText)
+   const newdepototal= previousdepoAmount  + newdepoAmount;
+   
 
-    deposittotal.innerText=newdepoAmount;
+    deposittotal.innerText=newdepototal;
+
+
+//update balance
+
+const balancetotal=document.getElementById('balance-total');
+const balancetotaltext=balancetotal.innerText;
+const previousbaltotal=parseFloat(balancetotaltext);
+const newbaltotal= previousbaltotal + newdepoAmount;
+balancetotal.innerText=newbaltotal;
+
     depoinput.value='';
 
 })
+
+
+// withdraw
+
+
 
 
 
